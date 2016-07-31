ef50dec297ccd73b5644e0aa6163f83afde87450
pandolia <pandolia@yeah.net>
2016-07-31 12:51:00 +0800

    modify setup.py's version'

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ from distutils.core import setup
 
 setup(
     name = 'qqbot',
-    version = '1.12',
+    version = '1.13',
     packages = ['qqbot'],
     scripts = ['qqbot/qqbot', 'qqbot/qqbot.py'],
     description = "QQBot: A conversation robot base on Tencent's SmartQQ",
     author = 'pandolia',
     author_email = 'pandolia@yeah.net',
     url = 'https://github.com/pandolia/qqbot/',
-    download_url = 'https://github.com/pandolia/qqbot/archive/v1.12.tar.gz',
+    download_url = 'https://github.com/pandolia/qqbot/archive/v1.13.tar.gz',
     keywords = ['QQBot', 'conversation robot', 'tencent', 'qq', 'web', 'network', 'python', 'http'],
     classifiers = [],
 )
