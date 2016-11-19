f96dccf778bc6fc861edfe80bf22d8868acfb61c
pandolia <pandolia@yeah.net>
2016-11-19 20:59:57 +0800

    fix readme.md

--- uploadtopip.sh
+++ uploadtopip.sh
@@ -2,3 +2,7 @@ python setup.py register -r pypitest
 python setup.py sdist upload -r pypitest
 python setup.py register -r pypi
 python setup.py sdist upload -r pypi
+rm -r dist
+rm -r qqbot.egg-info
+
+
