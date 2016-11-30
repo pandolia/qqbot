1c725424c90a67b3af1fff3090b8d5c525d1af4a
pandolia <pandolia@yeah.net>
2016-11-30 15:34:48 +0800

    remove username

--- email-test.py
+++ email-test.py
@@ -1,9 +1,8 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Tue Nov 29 11:51:42 2016
 
-@author: huang_cj2
-"""
+account = '???@163.com'
+auth_code = '???'
+smtp_server = 'smtp.163.com'
 
 from email.encoders import encode_base64
 from email.mime.multipart import MIMEMultipart
@@ -20,21 +19,16 @@ def format_addr(s):
         addr.encode('utf-8') if isinstance(addr, unicode) else addr
     ))
 
-from_addr = 'pandolia@163.com'
-auth_code = '???'
-to_addr = from_addr
-smtp_server = 'smtp.163.com'
-
 msg = MIMEMultipart()
-msg['From'] = format_addr(u'QQBOT管理员 <%s>' % from_addr)
-msg['To'] = format_addr(u'QQBOT管理员 <%s>' % to_addr)
+msg['From'] = format_addr(u'QQBOT管理员 <%s>' % account)
+msg['To'] = format_addr(u'QQBOT管理员 <%s>' % account)
 msg['Subject'] = Header(u'QQBOT 二维码登录', 'utf-8').encode()
 msg.attach(MIMEText(
     '<html><body><p><img src="cid:0"</p></body></html>', 'html', 'utf-8'
 ))
 
 
-with open('C:\\Users\\huang_cj2\\.qqbot-tmp\\qrcode-1479288297.133000.png', 'rb') as f:
+with open('C:\\Users\\xxx\\.qqbot-tmp\\qrcode-1479288297.133000.png', 'rb') as f:
     m = MIMEBase('image', 'png', filename='qrcode.png')
     m.add_header('Content-Disposition', 'attachment', filename='qrcode.png')
     m.add_header('Content-ID', '<0>')
@@ -43,11 +37,10 @@ with open('C:\\Users\\huang_cj2\\.qqbot-tmp\\qrcode-1479288297.133000.png', 'rb'
     encode_base64(m)
     msg.attach(m)
 
-server = smtplib.SMTP(smtp_server)
-server.starttls()
+server = smtplib.SMTP_SSL(smtp_server)
 server.set_debuglevel(1)
-server.login(from_addr, auth_code)
-server.sendmail(from_addr, to_addr, msg.as_string())
+server.login(account, auth_code)
+server.sendmail(account, account, msg.as_string())
 server.quit()
 
 
--- email-test2.py
+++ email-test2.py
@@ -1,39 +1,15 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Tue Nov 29 21:44:42 2016
 
-@author: huang_cj2
-"""
-
-import poplib
-# from email.parser import Parser
-
-email = 'pandolia@163.com'
+account = '???@163.com'
 auth_code = '???'
 pop3_server = 'pop3.163.com'
 
-server = poplib.POP3(pop3_server)
+import poplib
 
-print server.getwelcome()
-
-server.user(email)
+server = poplib.POP3_SSL(pop3_server)
+server.getwelcome()
+server.user(account)
 server.pass_(auth_code)
-
-print 'Messages: %s. Size: %s' % server.stat()
-
 resp, mails, octets = server.list()
-
-print mails
-
-index = len(mails)
-
-server.dele(index)
-
-# resp, lines, octets = server.retr(index)
-
-# msg_content = '\r\n'.join(lines)
-
-# msg = Parser().parsestr(msg_content)
-
+mails and server.dele(len(mails))
 server.quit()
-
